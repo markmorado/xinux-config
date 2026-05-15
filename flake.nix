543ed9fd1a5e5d859c72f5a9bae912cf25fec474
Dilshod Rasulov <20490215+markmorado@users.noreply.github.com>
2026-05-15 19:31:52 +0500

    Fix nixpkgs URL to point to NixOS repository

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:xinux-org/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nix-data = {
       url = "github:xinux-org/nix-data/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
